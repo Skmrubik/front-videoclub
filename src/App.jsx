0b559863ae58ab9efca88b3bcb5499ad5359962b
JORGE\jorge <skmrubik@gmail.com>
2025-07-29 13:32:36 +0200

    añadido filtro de peliculas por duracion

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,42 @@
-import { useState, useEffect } from 'react'
-import { getFirstNFilms } from './services/Films'
+import React, { useState, useEffect } from 'react'
+import { getFilmsBetweenLength } from './services/Films'
+import Slider from '@mui/material/Slider'
+import Box from '@mui/material/Box';
 import './App.css'
 
 function App() {
   const [posts, setPosts] = useState([]);
+  const [value, setValue] = useState([20, 37])
     
   useEffect(() => {
-    getFirstNFilms(5)
+    getFilmsBetweenLength(value[0],value[1])
       .then(items => {
         setPosts(items)
       })
       .catch((err) => {
         console.log(err.message);
       });
-    }, []
+    }, [value]
   );
   
+  const valuetext = (value) => {
+    return `${value}°C`;
+  }
+  const handleChange = (e, newValue) => {
+    setValue(newValue);
+  };
   return (
     <>
+      <Box sx={{ width: 300, marginTop: 5, marginLeft: 5}}>
+        <Slider
+          getAriaLabel={() => 'Temperature range'}
+          value={value}
+          onChange={handleChange}
+          valueLabelDisplay="auto"
+          getAriaValueText={valuetext}
+          max={300}
+        />
+      </Box>
       <ul className='cuadro'>
         {posts.map((item, value) => {
           return(
--- src/services/Films.js
+++ src/services/Films.js
@@ -2,3 +2,8 @@ export function getFirstNFilms(tam) {
     return fetch('http://localhost:8080/listFilms/'+tam)
         .then((response) => response.json())
 }
+
+export function getFilmsBetweenLength(min, max) {
+    return fetch('http://localhost:8080/listFilmsBetweenDuration?minValue='+min+'&maxValue='+max)
+        .then((response) => response.json())
+}
