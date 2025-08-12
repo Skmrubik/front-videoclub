d9622b2e3ca4916fcda9ef4c59f3c8859fc6e370
JORGE\jorge <skmrubik@gmail.com>
2025-08-12 12:52:04 +0200

    cambio de las rutas

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <div className="menu" >
       <div className="navbar" >
-        <NavLink to="/"
+        <NavLink to="/films"
                     style={({ isActive }) => ({
                         color: isActive? "black": "#595959",
                         fontWeight: "bold",
@@ -16,11 +16,11 @@ createRoot(document.getElementById('root')).render(
                         fontSize: "20px",
                         textDecoration: "none",
                         marginRight: "15px",
-                    })}>Home
+                    })}>Peliculas
         </NavLink>
       </div>
       <div className="navbar" >
-        <NavLink to="/about"
+        <NavLink to="/customers"
                   style={({ isActive }) => ({
                       color: isActive? "black": "#595959",
                       fontWeight: "bold",
@@ -28,13 +28,13 @@ createRoot(document.getElementById('root')).render(
                       fontSize: "20px",
                       textDecoration: "none",
                       marginRight: "15px",
-                  })}>About
+                  })}>Clientes
         </NavLink>
       </div>
     </div>
     <Routes>
-      <Route path="/" element={<App />} />
-      <Route path="/about" element={<AppNav />} />
+      <Route path="/films" element={<App />} />
+      <Route path="/customers" element={<AppNav />} />
     </Routes>
   </BrowserRouter>
 )
