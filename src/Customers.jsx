7421dc8f095527c847aa3ec7871645fcd7f4387f
JORGE\jorge <skmrubik@gmail.com>
2025-08-13 10:22:21 +0200

    validación del formulario de clientes acabada

--- src/Customers.jsx
+++ src/Customers.jsx
@@ -11,14 +11,15 @@ const actors = [
   { value: 3, label: "Actor 3" }
 ];
 function Customers() {
-  const { register, handleSubmit, formState: { errors } } = useForm();
+  const { register, handleSubmit, reset, formState: { errors } } = useForm();
   const [countries, setCountries] = useState([]);
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const [cities, setCities] = useState([]);
   const [selectedOption, setSelectedOption] = useState("");
 
   const onSubmit = (data) => {
-    const customer = {
+    if (Object.keys(errors).length === 0) {
+      const customer = {
                 firstName: data.name,
                 lastName: data.apellido,
                 email: data.email,
@@ -33,13 +34,19 @@ function Customers() {
                     phone: data.telefono,
                 }
             }
-    insertCustomer(customer)
-      .then((items) => {
-        console.log("Cliente insertado");
-      })
-      .catch((err) => {
-        console.log(err.message);
-      });
+      insertCustomer(customer)
+        .then(() => {
+          console.log("Cliente insertado");
+          alert("Cliente insertado correctamente");
+          // Reset form or redirect as needed 
+          reset();  
+          setSelectedOption("");
+          setCities([]);
+        })
+        .catch((err) => {
+          console.log(err.message);
+        });
+    }
   };
 
   useEffect(() => {
@@ -98,31 +105,37 @@ function Customers() {
         <p style={{marginTop: 40, fontSize: 15}}>Introduce los datos de un nuevo cliente</p>
         <div className="form-container">
           <div className="form-input"> 
-            <p className="form-label">Nombre</p>
+            <p className="form-label">Nombre*</p>
             <input 
-              {...register('name', { required: 'Nombre es obligatorio' })} 
+              {...register('name', { required: 'Nombre es obligatorio', pattern: {
+                  value: /^[a-zA-Z\s]{0,20}$/,
+                  message: 'Nombre debe tener como máximo 20 caracteres'
+                } })} 
             />
           </div>
           <div className="form-input"> 
-            <p className="form-label">Apellidos</p>
+            <p className="form-label">Apellidos*</p>
             <input 
-              {...register('apellido', { required: 'Apellidos es obligatorio' })} 
+              {...register('apellido', { required: 'Apellidos es obligatorio', pattern: {
+                  value: /^[a-zA-Z\s]{5,50}$/,
+                  message: 'Apellido debe tener entre 5 y 50 caracteres'
+                } })} 
             />
           </div>
           <div className="form-input"> 
-            <p className="form-label">Email</p>
+            <p className="form-label">Email*</p>
             <input 
               {...register('email', { 
                 required: 'Email obligatorio',
-                /* pattern: {
-                  value: /^S+@S+$/i,
+                pattern: {
+                  value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
                   message: 'Email inválido'
-                } */
+                } 
               })} 
             />
           </div>
           <div className="form-input"> 
-            <p className="form-label">Dirección 1</p>
+            <p className="form-label">Dirección 1*</p>
             <input 
               {...register('direccion1', { required: 'Dirección 1 es obligatoria' })} 
             />
@@ -137,9 +150,11 @@ function Customers() {
             <p className="form-label">Pais</p>
             <div style={{ padding: '0px 10px 10px 10px', fontFamily: 'Segoe UI'}}>
               <Select
-                style={{ color: 'black', padding: 10}}
+                style={{ color: 'black', padding: 10 }}
+                className="selectCountry"
                 aria-labelledby="Segoe UI"
                 inputId="Segoe UI"
+                placeholder="Selecciona una opción..." 
                 name="Segoe UI"
                 onMenuOpen={onMenuOpen}
                 onMenuClose={onMenuClose}
@@ -151,30 +166,37 @@ function Customers() {
             </div>
           </div>
           <div className="form-input"> 
-            <p className="form-label">Ciudad</p>
-            <select className="selectCity" placeholder={selectedOption== ""?"Selecciona pais": ""} disabled={selectedOption==""? true: false} 
+            <p className="form-label">Ciudad*</p>
+            <select className="selectCity" disabled={selectedOption==""? true: false} 
             {...register("ciudad", { required: 'Ciudad es obligatoria' })}>
+              <option value="" disabled selected>Selecciona un país</option>
               {cities.map((city) => (
                 <option key={city.value} value={city.value}>{city.label}</option>
               ))}
             </select>
           </div>
           <div className="form-input"> 
-            <p className="form-label">Distrito</p>
+            <p className="form-label">Distrito*</p>
             <input 
               {...register('distrito', { required: 'Distrito es obligatorio' })} 
             />
           </div>
           <div className="form-input">
-            <p className="form-label">Código postal</p>
+            <p className="form-label">Código postal*</p>
             <input 
-              {...register('codigoPostal', { required: 'Código postal es obligatorio' })} 
+              {...register('codigoPostal', { required: 'Código postal es obligatorio', pattern: {
+                value: /^\d{5}$/,
+                message: "El código postal debe tener 5 dígitos",
+              },})} 
             />
           </div>
           <div className="form-input">
-            <p className="form-label">Teléfono</p>
+            <p className="form-label">Teléfono*</p>
             <input 
-              {...register('telefono', { required: 'Teléfono es obligatorio' })} 
+              {...register('telefono', { required: 'Teléfono es obligatorio', pattern: {
+                value: /^\d{9}$/,
+                message: "El teléfono debe tener 9 dígitos",
+              },})} 
             />
           </div>
         </div>
@@ -184,7 +206,8 @@ function Customers() {
           {errors.email && <p className="error">{errors.email.message}</p>}
           {errors.direccion1 && <p className="error">{errors.direccion1.message}</p>}
           {errors.direccion2 && <p className="error">{errors.direccion2.message}</p>}
-          {errors.distrito && <p className="error">{errors.distrito.message}</p>}
+          {errors.ciudad && <p className="error">{errors.ciudad.message}</p>}
+          {errors.distrito && <p className="error">{errors.distrito.message}</p>}        
           {errors.codigoPostal && <p className="error">{errors.codigoPostal.message}</p>}
           {errors.telefono && <p className="error">{errors.telefono.message}</p>}
         </div>
